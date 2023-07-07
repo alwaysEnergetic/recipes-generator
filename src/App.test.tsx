30a34467acbaf91a4cf287d5ae15619f5a5367e0
alwaysEnergetic <dhorse443@gmail.com>
2023-07-07 16:24:25 -0400

    initial test added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('No Recipe Found', ()=>{
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/No Recipe Found/i);
   expect(linkElement).toBeInTheDocument();
-});
+})
