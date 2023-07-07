2b2e92c2e6d04d7aeea3889981ae6a4edf0432f6
alwaysEnergetic <dhorse443@gmail.com>
2023-07-07 14:16:38 -0400

    use recipe hook added

---
+++ src/hooks/useRecipe.ts
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Recipe } from "../types/types";
+import { searchRecipesService, fetchRecipeByIdService } from "../services/RecipeService";
+
+export default function useRecipe() {
+
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [selectedRecipe, setSelectedRecipe] = useState<Recipe>();
+
+    const searchRecipes = async (searchTerm: string) => {
+        const response = await searchRecipesService(searchTerm);
+        setRecipes(response);
+    }
+
+    const fetchRecipeById = async (id: string) => {
+        const response = await fetchRecipeByIdService(id);
+        setSelectedRecipe(response);
+    }
+
+    return {
+        recipes,
+        searchRecipes,
+        selectedRecipe,
+        fetchRecipeById
+    }
+}
--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -2,14 +2,14 @@ import { Recipe } from "../types/types";
 import { BASE_URL_API } from "../utils/constant";
 import { formatRecipe } from "../utils/helper";
 
-export async function searchRecipes(searchTerm: string): Promise<Recipe[]> {
+export async function searchRecipesService(searchTerm: string): Promise<Recipe[]> {
     const response = await fetch(`${BASE_URL_API}/filter.php?i=${searchTerm}`);
     const data = await response.json();
     return data.meals;
 }
 
 
-export async function fetchRecipeById(id: string): Promise<Recipe> {
+export async function fetchRecipeByIdService(id: string): Promise<Recipe> {
     const response = await fetch(`${BASE_URL_API}/lookup.php?i=${id}`);
     const data = await response.json();
     return formatRecipe(data.meals[0]);
